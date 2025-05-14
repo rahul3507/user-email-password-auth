0565b690d81635030ef3f9a115f5cd8074e997a4
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-14 13:51:41 +0600

    feat: add error handling for user registration

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,9 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React from 'react'
+import React, { useState } from 'react'
 import auth from '../../firebase/firebase.config';
 
 const Register = () => {
+  const[registerError, setRegisterError]=useState('');
   const handleRegister= (e)=>{
     e.preventDefault();
     const email = e.target.email.value;
@@ -14,35 +15,33 @@ const Register = () => {
     })
     .catch((error) => {
       console.log(error)
+      setRegisterError(error.message);
     });
   }
   return (
-    
-
-
-<div className="hero bg-base-200 ">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Register now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-        <form className='text-2xl flex flex-col gap-2.5 p-5' onSubmit={handleRegister}>
-          <label className="label">Email</label>
-          <input className='p-2 ' type="email" name='email' id='' />
-          <label className="label">Password</label>
-          <input className='p-2' type="password" name='password' id='' />
-         
-           <button className="btn btn-neutral mt-4" type='submit'>Register</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
+          <div className="hero bg-base-200 ">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+              <div className="text-center lg:text-left">
+                <h1 className="text-5xl font-bold">Register now!</h1>
+                <p className="py-6">
+                  Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                  quasi. In deleniti eaque aut repudiandae et a id nisi.
+                </p>
+              </div>
+              <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                  <form className='text-2xl flex flex-col gap-2.5 p-5' onSubmit={handleRegister}>
+                    <label className="label">Email</label>
+                    <input className='p-2 ' type="email" name='email' id='' />
+                    <label className="label">Password</label>
+                    <input className='p-2' type="password" name='password' id='' />
+                  
+                    <button className="btn btn-neutral mt-4" type='submit'>Register</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
   )
 }
 
