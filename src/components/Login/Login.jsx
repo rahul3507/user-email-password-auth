a98e80a9dcfaa52bd5d85acd4f7523ff33933835
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-18 14:15:41 +0600

    feat: implement login functionality with Firebase authentication and error handling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import { signInWithEmailAndPassword} from 'firebase/auth';
+import auth from "../../firebase/firebase.config"
+import React, { useState } from 'react'
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Login = () => {
+  const[registerError, setRegisterError]=useState('');
+    const [success,setSuccess]= useState('');
+    const[showPassword, setShowPassword]= useState(false);
+  const handleLogin= (e) =>{
+      e.preventDefault()
+      const  email = e.target.email.value;
+      const password =e.target.password.value;
+      console.log(email,password);
+
+      setRegisterError('');
+      setSuccess('');
+
+      signInWithEmailAndPassword(auth, email,password)
+            .then((result)=>{
+              console.log(result.user)
+              setSuccess('Sign in Successfully...')
+            })
+            .catch((error) => {
+                console.log(error)
+                setRegisterError(error.message);
+    });
+  }
   return (
     <div>
           <div className="hero bg-base-200 ">
@@ -14,14 +39,35 @@ const Login = () => {
               </div>
               <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
-                  <fieldset className="fieldset">
+                  <form className="fieldset" onSubmit={handleLogin}>
                     <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email" />
+                    <input type="email" name='email' className="input" placeholder="Email" />
                     <label className="label">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
+                    <div className='relative border justify-center'>
+                                            <input 
+                                                  className='w-full p-2' 
+                                                  type={showPassword? "text":"password" }
+                                                  name='password'
+                                                  id='' 
+                                                  />
+                                            <span 
+                                                onClick={()=>setShowPassword(!showPassword)}
+                                                className='cursor-pointer absolute top-3 right-4 '
+                                                >
+                                                  {
+                                                    showPassword ? <FaEyeSlash/>:<FaEye /> 
+                                                  }
+                                            </span>
+                    </div>
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-neutral mt-4">Login</button>
-                  </fieldset>
+                  </form>
+                   {
+                    registerError && <p className='text-red-400'>{registerError}</p>
+                  }
+                  {
+                    success && <p className='text-green-400'>{success}</p>
+                  }
                 </div>
               </div>
             </div>
